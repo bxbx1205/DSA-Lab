2fa7ead2697d73173cee4365577e320f0cbc96d2
Sarvesh Bhanarkar <srbhanarkar05@gmail.com>
2025-07-25 12:48:16 +0530

    lab 2 added

---
+++ LAB2/LinkedListExample.java
@@ -0,0 +1,15 @@
+package LAB2;
+import java.util.LinkedList;
+public class LinkedListExample {
+
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+
+        System.out.println(list);
+    }
+    
+}
